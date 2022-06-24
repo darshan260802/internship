96b33925e48bc55c96552dd1e1b0a15b926bbb2b
Darshan Patel <darshanpatel2608ce@gmail.com>
2022-06-24 09:25:27 +0530

    Added Some animations

---
+++ index.js
@@ -0,0 +1,57 @@
+window.onload = () => {
+    setTimeout(() => {
+        document.querySelector('#delay_show').style.fontSize = '18.72px';
+    },2000)
+}
+
+
+const sectionStats = new IntersectionObserver((entry) => {
+    let a = [...entry[0].target.children];
+    if(entry[0].isIntersecting) // Specilize div
+    {
+        a.forEach((item) => {
+            item.style.transform = "scale(1)";
+        })  
+    }else{
+        a.forEach((item) => {
+            item.style.transform = "scale(0) ";
+        })
+    }
+      
+})
+
+const specializeDiv = new IntersectionObserver((entry) => {
+    let a = [...entry[0].target.children];
+    if(entry[0].isIntersecting) // Specilize div
+    {
+        a.forEach((item) => {
+            item.style.opacity = "100%";
+        })  
+    }else{
+        a.forEach((item) => {
+            item.style.opacity = "0%";
+        })
+    }
+})
+
+const processDiv = new IntersectionObserver((entry) => {
+    let a = [...entry[0].target.children].map((item) => item.children[0]);
+    if(entry[0].isIntersecting) // Specilize div
+    {
+        a.forEach((item) => {
+            item.classList.remove("animate-process");
+        })  
+    }else{
+        a.forEach((item) => {
+            item.classList.add("animate-process");
+        })
+    }
+})
+
+sectionStats.observe(document.querySelector('.section-stats'));
+specializeDiv.observe(document.querySelector('.specialize-div'));
+processDiv.observe(document.querySelector('.process-div'));
+
+document.querySelector("#activity-btn").addEventListener('click', () => {
+    location = "activity.html"
+})
